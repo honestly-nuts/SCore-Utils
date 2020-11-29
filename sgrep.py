34c86d9e0986d790192680d78709135614416137
Honestly Nuts <honeastlynuts@gmail.com>
2020-11-29 08:50:35 +0600

    added options.
    I added options into the program. I don't how I made it work or why it
    works but it's good for now.

--- sgrep.py
+++ sgrep.py
@@ -3,14 +3,14 @@ import sys
 import re
 
 
-def grep_file(token):
-    for i in sys.argv[2:]:
-        with open(i, "r") as fl:
-            for line in fl.read().split("\n"):
-                for word in line.split(" "):
-                    if word == token:
-                        sys.stdout.write(line + "\n")
-
+def grep_file(f, token):
+    text = ""
+    with open(f, "r") as fl:
+        for line in fl.read().split("\n"):
+            for word in line.split(" "):
+                if word == token:
+                    text += line
+    return text
 
 def grep_stdin(token):
     for line in sys.stdin.read().split("\n"):
@@ -20,10 +20,33 @@ def grep_stdin(token):
 
 
 if __name__ == '__main__':
+    optlist = []
+
     if len(sys.argv) == 2:
         grep_stdin(sys.argv[1])
     elif len(sys.argv) >= 3:
-        grep_file(sys.argv[1])
+        text = ""
+        for arg in sys.argv[1:]:
+            if re.findall(r"^-", arg) != []:
+                optlist.append(arg)
+
+        # to desable the options when executed once
+        i_opt_defuse = False
+        n_opt_defuse = False
+
+        for arg in sys.argv[2:]:
+            if optlist != [] and arg not in optlist:
+                text += grep_file(arg, sys.argv[1])
+                if "-c" in optlist:
+                    print(len(text.split("\n")))
+                    n_opt_defuse = True
+                else:
+                    print("Option not found")
+
+            elif optlist == []:
+                print(grep_file(arg, sys.argv[1]))
+
     else:
         print("Usage: sgrep.py token file(s)")
         sys.exit()
+
