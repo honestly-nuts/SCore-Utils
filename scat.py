ded4adf3e41ebafd4718261b7473a20cfb55b32b
Honestly Nuts <honeastlynuts@gmail.com>
2020-11-29 22:29:32 +0600

    divided cat() function from scat.py into 2 functions and a few minor changes

--- scat.py
+++ scat.py
@@ -1,20 +1,29 @@
 #! /bin/python3.8
 import sys
 
+def stdinCat():
+    while True:
+        text = input()
+        sys.stdout.write(text)
+
+def fileCat(filename):
+    text = ""
+    with open(filename, "r") as f:
+        text += f.read()
+    return text
+
 def cat():
-    if len(sys.argv) < 2:
-        while True:
-            text = input()
-            print(text)
-    else:
-        text = ''
+    if len(sys.argv) <= 1:
+        stdinCat()
+    elif len(sys.argv) >= 2:
         for filename in sys.argv:
-            if (filename != sys.argv[0]):
-                with open(filename, "r") as f:
-                    sys.stdout.write(f.read())
+            if filename != sys.argv[0]:
+                print(fileCat(filename))
+
 
 if __name__ == '__main__':
     try:
         cat()
     except:
+        sys.stdout.write("\n")
         pass
