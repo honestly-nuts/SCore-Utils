cb76b402726464c62e13d821a69d6272a9a0f65e
Honestly Nuts <honeastlynuts@gmail.com>
2020-11-30 18:44:41 +0600

    fixed install.sh chmod error

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ then
 fi
 
 # I know theres better ways to do this but this works for now
-chmod +x *.py
+chmod +x src/*.py
 
-mv src/scpy.py ./bin/scpy
-mv src/scat.py ./bin/scat
-mv src/sgrep.py ./bin/sgrep
-mv src/syes.py ./bin/syes
-mv src/swhoami.py ./bin/swhoami
-mv src/sbase64.py ./bin/sbase64
-mv src/sls.py ./bin/sls
-mv src/sarch.py ./bin/sarch
+mv ./src/scpy.py ./bin/scpy
+mv ./src/scat.py ./bin/scat
+mv ./src/sgrep.py ./bin/sgrep
+mv ./src/syes.py ./bin/syes
+mv ./src/swhoami.py ./bin/swhoami
+mv ./src/sbase64.py ./bin/sbase64
+mv ./src/sls.py ./bin/sls
+mv ./src/sarch.py ./bin/sarch
