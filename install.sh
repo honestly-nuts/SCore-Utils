aa9c172b18861b998e816b2551ddd615b56a7cec
Sam Escott <6706111+sme272@users.noreply.github.com>
2020-11-30 17:30:47 +0100

    Fixed source file location

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 # I know theres better ways to do this but this works for now
 chmod +x *.py
 
-for FILE in ./*.py; do
+for FILE in ./src/*.py; do
     OUTPUT=${FILE#./}
     OUTPUT=${OUTPUT%.py}
     mv $FILE /bin/$OUTPUT
