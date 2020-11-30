04b789110648541db6bbdf23194db6be11f941af
Sam Escott <6706111+sme272@users.noreply.github.com>
2020-11-30 15:05:33 +0100

    Automated the install script

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-mkdir bin
-if [[ -f ~/.bashrc ]]
-then
+if [[ -f ~/.bashrc ]]; then
     echo "export PATH+=:$(pwd)/bin" >> ~/.bashrc
     source ~/.bashrc
 fi
 
 # I know theres better ways to do this but this works for now
-chmod +x src/*.py
+chmod +x *.py
 
-mv ./src/scpy.py ./bin/scpy
-mv ./src/scat.py ./bin/scat
-mv ./src/sgrep.py ./bin/sgrep
-mv ./src/syes.py ./bin/syes
-mv ./src/swhoami.py ./bin/swhoami
-mv ./src/sbase64.py ./bin/sbase64
-mv ./src/sls.py ./bin/sls
-mv ./src/sarch.py ./bin/sarch
+for FILE in ./*.py; do
+    OUTPUT=${FILE#./}
+    OUTPUT=${OUTPUT%.py}
+    mv $FILE /bin/$OUTPUT
+done    
