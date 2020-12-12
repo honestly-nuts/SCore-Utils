e27686b28fc5475d8f89af6b126faa3286df745e
Honestly Nuts <honeastlynuts@gmail.com>
2020-12-12 19:14:18 +0600

    added mkdir and rmdir

---
+++ src/rmdir.py
@@ -0,0 +1,16 @@
+#!/bin/python3.8
+import os
+import sys
+
+if len(sys.argv) == 1:
+    sys.stderr.write("Too few arguments\n")
+    sys.exit()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if os.path.exists(sys.argv[1]):
+            os.rmdir(sys.argv[1])
+    else:
+        for arg in sys.argv[1:]:
+            if os.path.exists(arg):
+                os.rmdir(arg)
